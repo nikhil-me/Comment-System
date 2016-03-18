9affeef7f677e8b74d841ebf449d08a438c136b1
someone12321 <mehandirattanikhil@gmail.com>
2016-03-19 04:31:53 +0530

    connection with database is made and angular is applied on comment listing

--- CommentSystem/public/javascripts/CommentController.js
+++ CommentSystem/public/javascripts/CommentController.js
@@ -1,13 +1,43 @@
 angular.module('commentApp')
-	.controller("CommentsController",["$http","$scope",function($http,$scope){
+	.controller("CommentsController",["$http","$scope","$timeout" ,function($http,$scope,$timeout){
 
 		$scope.comments=["abc","dfg"];
 		$scope.comment;
 		$scope.name;
 
 		$scope.postComment = function(){
-			alert($scope.comment +" "+ $scope.name);
+			// alert($scope.comment +" "+ $scope.name);
+			var details = {
+				"username" : $scope.name,
+				"body" : $scope.comment,
+				"upvote" : 0,
+				"downvote" : 0
+			};
+
+			$http.post("/insert",details)
+	         .success(function(data){
+	         	console.log(data);
+	         	$scope.fetch();
+	         	$scope.name="";
+	         	$scope.comment="";
+	         })
+	         .error(function(err){
+	                 console.log(err);
+	         });
 		}
+
+		$scope.fetch = function(){
+			$http.get("/fetch")
+	         .success(function(data){
+	         	console.log(data);
+	         	$scope.comments = data;
+	         })
+	         .error(function(err){
+	                 console.log(err);
+	         });
+		}
+
+		$timeout($scope.fetch);
 
 	}]);
 
--- CommentSystem/routes/index.js
+++ CommentSystem/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+var mongoose = require("mongoose"),
+	Schema = mongoose.Schema;
+
+/************************************************************
+* The commentSchema which is used to generate the Model
+*************************************************************/
+var commentSchema = mongoose.Schema({
+	username : String,
+	body : String,
+	upvote : Number,
+	downvote : Number
 });
+
+var comment = mongoose.model('Comment', commentSchema);
+
+/* GET home page. */
+router.post('/insert', function(req, res, next) {
+	console.log(req.body);
+  	comment.create(req.body,function(err,newComment){
+		if(err){
+			console.log(err);
+		}else{
+			res.send({"status" : "Ok"});
+		}
+	});
+});
+
+router.get('/fetch', function(req, res, next) {
+	comment.find()
+		.exec(function(err,result){
+			res.send(result);
+	});
+});
+
 
 module.exports = router;
